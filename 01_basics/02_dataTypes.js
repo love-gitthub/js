6188cde5feebdd58b8402cb0ef57de3d1cfb19b8
love-gitthub <153540978+love-gitthub@users.noreply.github.com>
2023-12-11 18:51:07 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";  //treat all js code as newer version
+// alert (3+3)  we r using nodejs,not browser
+//primitive data types
+let name="love"//string
+let age=22 //number  -- 2 to poer 53,bigint
+let isloggedIn=false //boolean
+
+//null -standalone value
+//undefined- value not assign
+//symbol- unique
+
+//object
+
+console.log(typeof "love");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
