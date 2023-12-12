eba0f745c4e029f6d1b14f03ee48f040d6e1065e
love-gitthub <153540978+love-gitthub@users.noreply.github.com>
2023-12-12 07:25:57 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,22 +1,38 @@
 //let score="33"
 //let score=null
 //let score= undefined
+/*
 let score=true
 
 console.log(typeof score);
 let valueInNumber=Number(score)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+*/
 //"33"=33
 //"33abc"-Nan
 //true=1
+
+
+
+
+
+/*
+let value=3
 
 
 let isloggedIn=1
 
 let booleanIsLoggedIn=Boolean(isloggedIn)
 console.log(booleanIsLoggedIn);
+*/
 
 //1 =true  0=false
 //" "=false
 //"love"=true
+
+//**************Operations***********************/
+//console.log("1"+2+2);
+//console.log(2+2+"2");
+
+consol.log( );
